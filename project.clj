199276fe64e6ed232a6daa4197cedcefad42e879
Ola Ellnestam <ola@agical.se>
2010-12-13 20:28:01 +0100

    Modified ignore and updated project spec

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A clojure program for creating invoices"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :main aginvoice.run)
