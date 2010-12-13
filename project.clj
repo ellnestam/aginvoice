652a21ade75863d3c5a331869a77d157087415f9
Ola Ellnestam <ola@agical.se>
2010-12-13 17:24:28 +0100

    Initial

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aginvoice "1.0.0-SNAPSHOT"
+  :description "A clojure program for creating invoices"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+
+  :dev-dependencies [[swank-clojure "1.2.1"]])
