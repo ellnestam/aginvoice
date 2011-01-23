5c89d378898249c9597c58d60b2c6a3c65e2d412
Joakim Ohlrogge <joakim.ohlrogge@gmail.com>
2011-01-23 02:31:35 +0100

    seq-reader can read with length and offset

--- src/aginvoice/io.clj
+++ src/aginvoice/io.clj
@@ -8,17 +8,28 @@
 (unfinished )
 
 
-(defn copy-seq-to-array
-  ([s a] (copy-seq-to-array s a 0))
-  ([s a i] (if (empty? s)
-	     i
-	     (let [c (first s)]
-	       (aset a i (char c))
-	       (recur (rest s) a (inc i))))))
+(defn- copy-seq-to-array
+  ([s a] (copy-seq-to-array s a 0 (alength a)))
+  ([s a off len] (copy-seq-to-array s a off len 0))
+  ([s a off len idx] (if (or (empty? s) (= 0 len))
+	       idx
+	       (let [c (first s)]
+		 (aset a (+ off idx) (char c))
+		 (recur (rest s) a off (dec len) (inc idx))))))
+
+;.;. Any intelligent fool can make things bigger, more complex, and more
+;.;. violent. It takes a touch of genius -- and a lot of courage -- to move
+;.;. in the opposite direction. -- Schumacher
+
 (facts "about copy-seq-to-array"
        (let [array (make-array Character/TYPE 10)]
 	     (copy-seq-to-array (seq "hello") array) => (.length "hello")
-	     (String. array) => (has-prefix "hello")))
+	     (String. array) => (has-prefix "hello"))
+       (let [array (make-array Character/TYPE 11)]
+	 (copy-seq-to-array (seq " hello") array 5 6) => 6
+	 (copy-seq-to-array (seq "world") array 0 10) => 5
+	 (String. array) => "world hello"))
+       
 
 
 (defn seq-reader [s]
@@ -31,9 +42,11 @@
 		    (int res))))
 	    ([chars] (let [res (take (alength chars) @cursor)]
 		       (swap! cursor #(drop (alength chars) %))
-		       (copy-seq-to-array res chars)
-		       (count res)
-		     	    ))))))
+		       (copy-seq-to-array res chars)))
+	    ([chars off len] (let [res (take len @cursor)]
+			       (swap! cursor #(drop len %))
+			       (copy-seq-to-array res chars off len)))))))
+
 
 
 (facts "about seq-reader"
@@ -43,11 +56,15 @@
 	    (.read r) => (int \c)
 	    (.read r) => (int -1))
        (let [r (seq-reader (seq "hello world")) arr (make-array Character/TYPE 6)]
-	 (do (.read r arr) => 6
+	 (.read r arr) => 6
 	     (String. arr) => "hello "
 	     (.read r arr) => 5
-	     (String. arr) => "world "
-	     )))
+	     (String. arr) => "world ")
+       (let [r (seq-reader (seq "goodbye cruel world")) arr (make-array Character/TYPE 19)] 
+	  (.read r arr 12 7) => 7
+	  (.read r arr 11 1) => 1
+	  (.read r arr 0 11) => 11
+	  (String. arr) => "cruel world goodbye"))
 
 (extend StringSeq
   IOFactory
